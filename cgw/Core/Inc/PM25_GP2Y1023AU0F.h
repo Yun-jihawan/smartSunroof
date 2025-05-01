0e28ecf3279c87eedd47bf7a6b7955d05c60f4ac
yerin <lyt0330u@gmail.com>
2025-05-02 00:05:48 +0900

    fix: 먼지센서 micros() 함수 추가, 센싱값 연산 수정 [SZ4-64]

--- cgw/Core/Inc/PM25_GP2Y1023AU0F.h
+++ cgw/Core/Inc/PM25_GP2Y1023AU0F.h
@@ -2,6 +2,7 @@
 #define __SHARP_DUST_SENSOR_H
 
 #include "stm32l0xx_hal.h"
+#include "tim.h"
 
 #include <stdio.h>
 
@@ -12,10 +13,11 @@ typedef struct
     float         concentration;
 } sharp_dust_sensor_t;
 
-void  sharp_dust_sensor_init(sharp_dust_sensor_t *sensor,
-                             GPIO_TypeDef        *port,
-                             uint16_t             pin);
-void  sharp_dust_sensor_scan(sharp_dust_sensor_t *sensor);
-float sharp_dust_sensor_get_concentration(sharp_dust_sensor_t *sensor);
+uint32_t micros(void);
+void     sharp_dust_sensor_init(sharp_dust_sensor_t *sensor,
+                                GPIO_TypeDef        *port,
+                                uint16_t             pin);
+void     sharp_dust_sensor_scan(sharp_dust_sensor_t *sensor);
+float    sharp_dust_sensor_get_concentration(sharp_dust_sensor_t *sensor);
 
 #endif
