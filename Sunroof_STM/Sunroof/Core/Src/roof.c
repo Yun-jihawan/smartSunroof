86b37bc6234b29616c4064b3430075530c0450f6
dkstkddn <frommark@naver.com>
2025-05-06 20:34:01 +0900

    feat: 2개 모터 사용을 위한 데모 코드 추가

--- Sunroof_STM/Sunroof/Core/Src/roof.c
+++ Sunroof_STM/Sunroof/Core/Src/roof.c
@@ -7,6 +7,8 @@
 
 #include "roof.h"
 
+#define TWO_MOTOR 1
+#define ONE_MOTOR 0
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	uint8_t ROOF_A_state = 0;
@@ -39,6 +41,106 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	}
 }
 
+#if TWO_MOTOR
+void Sunroof_Set(uint8_t mode) {
+	switch(mode) {
+	case OPEN:
+		if(tilting_encoder >= 0)
+		{
+			// STOP Roof Motor
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+			// ACW Tilting Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, MOVE_SPEED);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 0);
+			HAL_GPIO_WritePin(TILTING_DIR_GPIO_Port, TILTING_DIR_Pin, ACW);
+		}
+		else if(roof_encoder <= ROOF_OPEN_MAX) {
+			// STOP Tilting Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+			// CW Roof Motor
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, MOVE_SPEED);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 0);
+			HAL_GPIO_WritePin(ROOF_DIR_GPIO_Port, ROOF_DIR_Pin, CW);
+		}
+		else {
+			// STOP Both Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+		}
+		break;
+	case TILTING:
+		if(roof_encoder >= 0) {
+			// STOP Tilting Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+			// ACW Roof Motor
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, MOVE_SPEED);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 0);
+			HAL_GPIO_WritePin(ROOF_DIR_GPIO_Port, ROOF_DIR_Pin, ACW);
+		}
+		else if(tilting_encoder <= ROOF_TILTING_MAX) {
+			// STOP Roof Motor
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+			// CW Tilting Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, MOVE_SPEED);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 0);
+			HAL_GPIO_WritePin(TILTING_DIR_GPIO_Port, TILTING_DIR_Pin, CW);
+		}
+		else {
+			// STOP Both Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+		}
+		break;
+	case CLOSE:
+		if(tilting_encoder >= 0)
+		{
+			// STOP Roof Motor
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+			// ACW Tilting Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, MOVE_SPEED);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 0);
+			HAL_GPIO_WritePin(TILTING_DIR_GPIO_Port, TILTING_DIR_Pin, ACW);
+		}
+		else if(roof_encoder >= 0) {
+			// STOP Tilting Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+			// ACW Roof Motor
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, MOVE_SPEED);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 0);
+			HAL_GPIO_WritePin(ROOF_DIR_GPIO_Port, ROOF_DIR_Pin, ACW);
+		}
+		else {
+			// STOP Both Motor
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+			HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+		}
+		break;
+	case STOP:
+	default:
+		// STOP Both Motor
+		__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+		HAL_GPIO_WritePin(TILTING_BRAKE_GPIO_Port, TILTING_BRAKE_Pin, 1);
+		__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+		HAL_GPIO_WritePin(ROOF_BRAKE_GPIO_Port, ROOF_BRAKE_Pin, 1);
+		break;
+	}
+}
+#endif
+
+
+#if ONE_MOTOR
 void Sunroof_Set(uint8_t mode) {
 	switch(mode) {
 	case OPEN:
@@ -81,3 +183,4 @@ void Sunroof_Set(uint8_t mode) {
 		break;
 	}
 }
+#endif
